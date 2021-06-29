dd73381232db8233f5246b60448234e22d6d3628
enochgao <enochgao@qq.com>
2021-06-29 15:31:24 +0800

    feat; adapter

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,48 @@
+console.group('创建型');
+
+console.groupCollapsed('单例模式');
 import singletonMain from './singleton';
-console.group('单例模式');
 singletonMain();
 console.groupEnd();
 
-import simpleFactoryMain from './simple-factory';
 console.groupCollapsed('简单工厂');
+import simpleFactoryMain from './simple-factory';
 simpleFactoryMain();
 console.groupEnd();
 
-import factoryMethodMain from './factory-method';
 console.groupCollapsed('工厂方法');
+import factoryMethodMain from './factory-method';
 factoryMethodMain();
 console.groupEnd();
 
-import abstractFactoryMain from './abstract-factory';
 console.groupCollapsed('抽象工厂');
+import abstractFactoryMain from './abstract-factory';
 abstractFactoryMain();
 console.groupEnd();
 
-import builderClientMain from './builder';
 console.groupCollapsed('建造者');
+import builderClientMain from './builder';
 builderClientMain();
 console.groupEnd();
 
-import prototypeMain from './prototype';
 console.groupCollapsed('原型模式');
+import prototypeMain from './prototype';
 prototypeMain();
+console.groupEnd();
+
+console.groupEnd();
+
+
+
+
+
+console.group('结构型');
+
+console.groupCollapsed('适配器模式');
+import adapterMain from './adapter';
+adapterMain();
+console.groupEnd();
+
+
+
 console.groupEnd();
